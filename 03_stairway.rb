9c90867220a7e7b350a998f19af7de2e13b809d9
Nicolas <52452559+NicolasDelhopital@users.noreply.github.com>
2019-07-11 23:10:51 +0200

    Add files via upload

---
+++ 03_stairway.rb
@@ -0,0 +1,23 @@
+puts "Pour monter les marches, veuillez lancer le dé."
+dice = 1 + rand(6)
+
+	while dice = 5 or 6
+		puts "Vous avancez d'une marche."
+		upwards_1 = 
+	end
+
+	while dice = 1
+		puts "Vous reculez d'une marche."
+		downwards_1 = 
+	end
+
+	while dice = 2 or 3 or 4
+		puts "Vous restez sur votre marche."
+		nothing = 
+	end
+
+def average_finish_time
+puts "Maintenant 100 parties vont être simulées."
+end
+
+
